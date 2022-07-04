c641d0fa4edae78e524f792ef16c3971db50dcc8
Huguette <yadidmishkan@gmail.com>
2022-07-04 14:22:46 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints different combinations between 00 to 89
+ * main - Prints different numbers between 00 to 89
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i; e;
-
+               
 	i = 48;
 	e = 48;
 
