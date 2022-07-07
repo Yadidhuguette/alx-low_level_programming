4456f90b1e683396ce71cf113fc4719e82f52174
Huguette <yadidmishkan@gmail.com>
2022-07-07 14:56:33 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of even valued fibonacci numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int x, y, xy, nxy;
+
+	x = 1;
+	y = 2;
+	xy = nxy = 0;
+	while (xy <= 4000000)
+	{
+		xy = x + y;
+		x = y;
+		y = xy;
+		if ((x % 2) == 0)
+		{
+			nxy += x;
+		}
+	}
+		printf("%ld\n", nxy);
+		return (0);
+}
+	
