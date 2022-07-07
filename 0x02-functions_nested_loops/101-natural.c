e139d63ad7a9a8084c20f5bc11d9e362bc62a89d
Huguette <yadidmishkan@gmail.com>
2022-07-07 14:15:46 -0700

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints list of all natural numbers below 1024
+ * multiples of 3 or 5.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{ int n, m;
+
+	for (n =1; n < 1024; n++)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+			m += n;
+	}
+	printf("%d\n", m);
+	return (0);
+}
