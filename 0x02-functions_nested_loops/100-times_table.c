af9fcabb7c6a84234261f8fb574e7b378a5ee9fe
Huguette <yadidmishkan@gmail.com>
2022-07-07 13:39:40 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,11 +31,11 @@ void print_times_tables(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-					_putchar(((pr0d /10)) % 10 + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar((prod /10) + '0');
+					_putchar((prod / 10) + '0');
 				}
 				_putchar((prod % 10) + '0');
 			}
