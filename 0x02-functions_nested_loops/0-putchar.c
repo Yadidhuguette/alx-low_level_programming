e8397502daf74b1ec6f8fe8abdcb9aac56c36517
Huguette <yadidmishkan@gmail.com>
2022-07-07 04:26:15 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main - Prints putchar with an new line.
+ * main - Prints _putchar with an new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	_putchar('_');
 	_putchar('p');
 	_putchar('o');
 	_putchar('u');
