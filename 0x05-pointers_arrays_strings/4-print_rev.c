8534862956f5ab51c51d34d97069a81a7c7eb6ed
Huguette <yadidmishkan@gmail.com>
2022-07-12 13:35:31 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: input string
+ */
+void print_rev(char *s)
+{
+	int num = 0;
+
+	while (num >= 0)
+	{
+		if (s[num] == '\0')
+			break;
+		num++;
+	}
+
+	for (num--; num >= 0; num--)
+		_putchar(s[num]);
+	_putchar('\n');
+}
