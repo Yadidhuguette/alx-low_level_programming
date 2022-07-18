91db9f4473f5fd21468391aa1fd18fe805d999b3
Huguette <yadidmishkan@gmail.com>
2022-07-18 04:38:34 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,28 +3,30 @@
 /**
  * cap_string - capitalizes all words of a string
  * @str: string to be capitalized
- * Return: result
+ * Return: the string
  */
 char *cap_string(char *str)
 {
-	int num = 0, i;
-	int sw[] = (32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125);
+	int count = 0;
 
-	if (*(str + num) >= 97 && *(str + num) <= 122)
-		*(str + num) = *(str + num) - 32;
-	num++;
-	while (*(str + num) != '\0')
+	while (str[++count])
 	{
-		for (i = 0; i < 13; i++)
-		{
-			if (*(str + num) == sw[i])
-			{
-				if ((*(str + (num + 1)) >= 97) && (*(str + (num + 1)) <= 122))
-					*(str + (num + 1)) = *(str + (num + 1)) - 32;
-				break;
-			}
-		}
-		num++;
+		while (!(str[count] >= 'a' && str[count] <= 'z'))
+			count++;
+		if (str[count - 1] == ' ' ||
+				str[count - 1] == '\t' ||
+				str[count - 1] == '\n' ||
+				str[count - 1] == ',' ||
+				str[count - 1] == ';' ||
+				str[count - 1] == '.' ||
+				str[count - 1] == '!' ||
+				str[count - 1] == '?' ||
+				str[count - 1] == '"' ||
+				str[count - 1] == '(' ||
+				str[count - 1] == ')' ||
+				str[count - 1] == '{' ||
+				str[count - 1] == '}')
+			str[count] -= 32;
 	}
 	return (str);
 }
