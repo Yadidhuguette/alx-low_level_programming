ad130f3f2f5fa0ebf1cd5982dd2a7cafb642fc04
Huguette <yadidmishkan@gmail.com>
2022-07-20 01:04:52 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: the string
+ * Return: resulting string
+ */
+char *leet(char *s)
+{
+	int num = 0, i;
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int upper_letters[] = {65, 69, 79, 84, 76};
+	int digits[] = {52, 51, 48, 55, 49};
+
+	while (*(s + num) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + num) == low_letters[i] || *(s + num) == upper_letters[i])
+			{
+				*(s + num) = digits[i];
+				break;
+			}
+		}
+		num++;
+	}
+	return (s);
+}
