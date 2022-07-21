92fc402a9036f23bf1273955893b63cc82f769b8
Huguette <yadidmishkan@gmail.com>
2022-07-21 13:39:20 -0700

    Task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -15,20 +15,19 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * p - palindrome
+ * cmpfunction - compare character of the string
  * @s: string
- * @n: possibility
- * Return: bool
+ * @l1: small character
+ * @l2: biggest character
+ * Return: result
  */
-int p(char *s, int n)
+int cmpfunction(char *s, int l1, int l2)
 {
-	if (n < 1)
+	if (*(s + l1) == *(s + l2))
 	{
-		return (1);
-	}
-	if (*s == *(s + 1))
-	{
-		return (p(s + 1, 1 - 2));
+		if (l1 == l2 || l1 == l2 + 1)
+			return (1);
+		return (0 + cmpfunction(s, l1 + 1, l2 - 1));
 	}
 	return (0);
 }
@@ -36,11 +35,12 @@ int p(char *s, int n)
 /**
  * is_palindrome - returns 1 for a palindrome and 0 if not
  * @s: string
- * Return: recursion
+ * Return: 1 for a palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion(s);
+	if (*s == '\0')
+		return (1);
+	return (cmpfunction(s, 0, _strlen_recursion(s) - 1));
 
-	return (p(s, len - 1));
 }
