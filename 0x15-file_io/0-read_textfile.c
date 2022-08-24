db24d40bf69f652dc12f1781ab70a1b6cf7dcea8
Huguette <yadidmishkan@gmail.com>
2022-08-23 23:49:27 -0700

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * read_textfile -  reads a text file and prints it 
+ * @filename: file's name
+ * @letters: letters to print
+ *
+ * Return: letters, 0 if failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	int lt;
+	ssize_t rlt, wlt;
+
+	if (!filename)
+		return (0);
+	
+	lt = open(filename, O_RDONLY);
+
+	if (lt == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+		return (0);
+
+	rlt = read(lt, buffer, letters);
+	wlt = write(STDOUT_FILENO, buffer, rlt);
+
+	close(lt);
+
+	free(buffer);
+
+	return (wlt);
+}
+
+	
+
+
