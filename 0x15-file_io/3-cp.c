5fc58731afdee2964c2664a9949bdcebd3c95ce2
Huguette <yadidmishkan@gmail.com>
2022-08-26 12:08:49 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -50,8 +50,8 @@ void close_file(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int from, to, r, w;
-	char *buffer;
+	int bg, dst, r, w;
+	char *buf;
 
 	if (argc != 3)
 	{
@@ -59,37 +59,37 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buf = create_buffer(argv[2]);
+	bg = open(argv[1], O_RDONLY);
+	r = read(bg, buf, 1024);
+	dst = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (from == -1 || r == -1)
+		if (bg == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
+			free(buf);
 			exit(98);
 		}
 
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
+		w = write(dst, buf, r);
+		if (dst == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
-			free(buffer);
+				"Error: Can't write dst %s\n", argv[2]);
+			free(buf);
 			exit(99);
 		}
 
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
+		r = read(bg, buf, 1024);
+		dst = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (r > 0);
 
-	free(buffer);
-	close_file(from);
-	close_file(to);
+	free(buf);
+	close_file(bg);
+	close_file(dst);
 
 	return (0);
 }
